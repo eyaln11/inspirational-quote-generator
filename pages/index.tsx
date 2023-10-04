0f63d478b67f4b670c9446b9da44e2ba2fe66040
Eyal Nahum <eyalnahom@gmail.com>
2023-10-04 13:45:36 +0300

    add api + lambda + auth + graphql

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 import Head from "next/head";
-import { GradientBackgroundCon } from "@/components/QuoteGenerator/QuoteGeneratorElements";
+import {
+  BackgroundImage1,
+  BackgroundImage2,
+  FooterCon,
+  FooterLink,
+  GenerateQuoteButton,
+  GenerateQuoteButtonText,
+  GradientBackgroundCon,
+  QuoteGeneratorCon,
+  QuoteGeneratorInnerCon,
+  QuoteGeneratorSubTitle,
+  QuoteGeneratorTitle,
+} from "@/components/QuoteGenerator/QuoteGeneratorElements";
+
+import Clouds1 from "../assets/cloud-and-thunder.png";
+import Clouds2 from "../assets/cloudy-weather.png";
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <Head>
@@ -10,7 +29,46 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <GradientBackgroundCon></GradientBackgroundCon>
+      <GradientBackgroundCon>
+        {/* <QuoteGeneratorModal /> */}
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+            <QuoteGeneratorTitle>
+              Daily Inspiration Generator
+            </QuoteGeneratorTitle>
+
+            <QuoteGeneratorSubTitle>
+              Looking for a splash of inspiration? Generate a quote card with a
+              random inspirational quote provided by{" "}
+              <FooterLink
+                href="https://zenquotes.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ZenQuotes API
+              </FooterLink>
+              .
+            </QuoteGeneratorSubTitle>
+
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText onClick={() => {}}>
+                Make a quote
+              </GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+        <BackgroundImage1 src={Clouds1} height="300" alt="cloudybackground1" />
+        <BackgroundImage2 src={Clouds2} height="300" alt="cloudybackground2" />
+
+        <FooterCon>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed by 111
+          </>
+        </FooterCon>
+      </GradientBackgroundCon>
     </>
   );
 }
